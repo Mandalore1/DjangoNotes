7e2832480ff7ee0421114419e634854359712659
Mandalore <serriy_volk@mail.ru>
2022-06-02 19:05:37 +0500

    Add: Registration

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,15 @@
 import django.forms as forms
+from django.contrib.auth.models import User
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(label="Имя пользователя", widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={"class": "form-control"}))
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    password_confirmation = forms.CharField(label="Подтверждение пароля",
+                                            widget=forms.PasswordInput(attrs={"class": "form-control"}))
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label="Имя пользователя", widget=forms.TextInput())
-    password = forms.CharField(label="Пароль", widget=forms.PasswordInput())
+    username = forms.CharField(label="Имя пользователя", widget=forms.TextInput(attrs={"class": "form-control"}))
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={"class": "form-control"}))
--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.db import DatabaseError
 from django.shortcuts import render, redirect
 
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 
 
 # Create your views here.
@@ -38,3 +40,39 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("login")
+
+
+def register_view(request):
+    register_form = None
+
+    if request.method == "GET":
+        register_form = RegisterForm()
+
+    if request.method == "POST":
+        register_form = RegisterForm(data=request.POST)
+        if register_form.is_valid():
+            if register_form.cleaned_data["password"] != register_form.cleaned_data["password_confirmation"]:
+                messages.error(request, "Пароль и подтверждение пароля не совпадают!")
+                return render(request, "register.html", {"form": register_form})
+
+            same_username = User.objects.filter(username=register_form.cleaned_data["username"]).exists()
+            if same_username:
+                messages.error(request, "Имя пользователя уже существует!")
+                return render(request, "register.html", {"form": register_form})
+
+            try:
+                user = User.objects.create_user(register_form.cleaned_data["username"],
+                                                register_form.cleaned_data["email"],
+                                                register_form.cleaned_data["password"])
+            except DatabaseError:
+                messages.error(request, "Не удалось зарегистрироваться!")
+                return render(request, "register.html", {"form": register_form})
+            else:
+                login(request, user)
+                messages.success(request, "Вы успешно зарегистрировались!")
+                return redirect("test")
+
+        else:
+            messages.error(request, "Данные имели неверный формат")
+
+    return render(request, "register.html", {"form": register_form})
