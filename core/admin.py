7b33d46866b9943e5534b5beddab56c80d3f16b9
Mandalore <serriy_volk@mail.ru>
2022-06-15 13:36:10 +0500

    Add: tags in notes

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from .models import Note
+from .models import Note, Tag
 
 # Register your models here.
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
 admin.site.register(Note)
+admin.site.register(Tag, TagAdmin)
--- core/models.py
+++ core/models.py
@@ -4,14 +4,28 @@ from django.db import models
 # Create your models here.
 
 
+class Tag(models.Model):
+    """Модель тега"""
+    name = models.CharField(max_length=150, verbose_name="Название", unique=True)
+    slug = models.SlugField(max_length=150, verbose_name="Slug", unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+
+
 class Note(models.Model):
-    """Класс записки"""
+    """Модель записки"""
     title = models.CharField(max_length=150, verbose_name="Заголовок")
     text = models.TextField(verbose_name="Текст", blank=True, null=True)
     image = models.ImageField(upload_to="note_images", verbose_name="Изображение", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создан")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлен")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes", verbose_name="Пользователь")
+    tags = models.ManyToManyField(Tag, blank=True, related_name="notes", verbose_name="Теги")
 
     def __str__(self):
         return self.title
